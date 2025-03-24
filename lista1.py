820a960bc5c6c8ef8f0de94c5d71af94f6bae552
Edudsprado <eduardodsprado123@gmail.com>
2025-03-24 15:24:50 +0000

    Feita q4-11

--- lista1.py
+++ lista1.py
@@ -6,12 +6,9 @@ Exercícios sobre os comandos básicos em Python
 def q01():
     print('Eduardo dos santos prado')
 
-
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
-
-
- def q02():
-...     print(30*27)
+def q02():
+    print(30*27)
 
 
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
@@ -20,29 +17,74 @@ def q03():
 
 #4. Faça um programa que leia e imprima um número inteiro.
 def q04():
-    print (input("Digite um número inteiro: "))
-
+    print = int(input("Digite um número inteiro: "))
 #5. Faça um programa que leia dois números reais e os imprima.
+def q05():
+    num1 = float (input("Digite um número real: "))
+    num2= float (input("Digite um número real: "))
+    print (num1)
+    print(num2)   
 
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
+def q06():
+    ant = int (input("Digite um número inteiro"))
+    print(ant-1)
+    print(ant+1)
 
 #7. Faça um programa que leia o nome o endereço e o telefone de
 #   um cliente e ao final, imprima esses dados.
-
+def q07():
+    nome = (input("DIgite seu nome:"))
+    endereco = (input("Digite seu endereço"))
+    tel = int(input("Digite seu telefone"))
+    texto = f'''
+        Nome: {nome}
+        Endereço: {endereco}
+        telefone: {tel}
+    '''
+    print(texto)
 #8. Faça um programa que leia dois números inteiros e imprima a
 #   subtração deles.
+def q08():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digitte o segundo número: "))
+    print (num1-num2)
+
 
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
+def q09():
+    num = float(input("Digite um número: "))
+    print(num/4)
 
 #10. Faça um programa que leia três números reais e calcule a
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
+def q10():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    num3 = float(input("Digite o terceiro número: "))
+    media = ((num1+num2+num3)/3)
+    print(media)
 
 #11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
+
+def q11():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    soma = num1+num2
+    sub = num1 - num2
+    mult = num1 * num2
+    div = num1 / num2
+    print(soma)
+    print(sub)
+    print(mult)
+    print(div)
+q11()
+
 
 #12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
