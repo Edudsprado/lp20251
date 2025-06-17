5ed8351b8a78468cc43e45b46ed5702ef34b4e01
Edudsprado <eduardodsprado123@gmail.com>
2025-06-17 14:30:45 +0000

    Criar mais

---
+++ Biblio.py
@@ -0,0 +1,15 @@
+def input_float(msg, min, max):
+    error = True
+    numero = 0
+    while error == True:
+        try:
+            numero = float(input(msg))
+            if numero < min or numero > max:
+                print(f'Valor informado fora do intervalo permitido {min}-{max}')
+            else:
+                error = False
+        except ValueError:
+            print('O valor informado não é um número Real!')
+        except:
+            print('Erro desconhecido. Entre em contato com o admin do sistema!')
+    return numero
